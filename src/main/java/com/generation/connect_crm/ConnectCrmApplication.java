e5c7f27084462a305cd755c539bb7526ab954a16
luizsantos7 <lhsantos098@gmail.com>
2025-05-30 09:40:59 -0300

    :tada: commit inicial + configuração do banco de dados

---
+++ src/main/java/com/generation/connect_crm/ConnectCrmApplication.java
@@ -0,0 +1,13 @@
+package com.generation.connect_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/connect_crm/ConnectCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.connect_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
