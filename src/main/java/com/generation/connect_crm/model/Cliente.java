df44d0032d4ff025dfb418058accf843587be4da
Lucas Manrick <lucasmanrick.ipms@gmail.com>
2025-05-30 10:58:00 -0300

    :sparkles: adicionando estrutura do projeto

---
+++ src/main/java/com/generation/connect_crm/model/Cliente.java
@@ -0,0 +1,47 @@
+package com.generation.connect_crm.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_clientes")
+public class Cliente{
+	private Long id;
+	private String nome;
+	private String email;
+	private String telefone;
+	private String interesse;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getInteresse() {
+		return interesse;
+	}
+	public void setInteresse(String interesse) {
+		this.interesse = interesse;
+	}
+	
+//	@oneToMany
+//	private List<Oportunidade> oportunidade;
+}
---
+++ src/main/java/com/generation/connect_crm/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.generation.connect_crm.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.connect_crm.model.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	List<Cliente> findAllByEmailContainingIgnoreCase(String email);
+}
